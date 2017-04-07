2e4a2db3bc457682b5d31cb76baa127a556a9a8f
Suelando Alves <suelandoalves@live.com>
2017-04-06 22:39:42 -0300

    Removido

--- projeto/users-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "database.php";
-class CreateUsersDb extends Database {
-   public function createUser($formUser, $formPassword){//Função que criar o usuário.
-    $sql = "  insert into users_routers(status_user,name_user,password)".
-            " values(1,'{$formUser}','{$formPassword}');";
-    $this->connection->exec($sql);
-  }
-}
